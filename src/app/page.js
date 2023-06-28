caef5f0420221be336b3335f0bbc744ee328ffec
Abuzar Khan <abuzarghazanfarkhanagk@gmail.com>
2023-06-28 22:09:41 +0500

    OasisX landing

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,114 @@
+"use client";
+
 import Image from 'next/image'
-import styles from './page.module.css'
+
+import styles from './page.module.scss'
+// import "./App.css";
+import { useState,useEffect } from 'react';
+import logo from "./images/Logo2.svg";
+import palm from "./images/Palm.svg";
+import arrow from "./images/arrow white and red.svg"
+import arrowWhite from "./images/arrow white and white.svg"
+import {
+  FaDiscord,
+  FaInstagram,
+  FaLinkedin,
+  FaTelegramPlane,
+  FaTwitter,
+  FaYoutube,
+} from 'react-icons/fa';
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+  const [hoverLeft, setHoverLeft] = useState(false)
+const [hoverRight, setHoverRight] = useState(false)
+useEffect(() => {
+  // console.log(hoverLeft);
+}, [hoverLeft,hoverRight])
+
+return (
+  <div className={`${styles.App}`}>
+    
+    <center>
+      <Image style={{marginTop:"25px",maxWidth:"90px",maxHeight:"80px"}} src={logo} />
+    </center>
+    <h1 className={`${styles.h1}`}>
+      SCULPTING A NEW OASIS <br />
+      AMIDST THE SANDS OF PROGRESS
+    </h1>
+    <center>
+      <div className={`${styles.main}`}>
+        <div
+          className={`${styles.box} ${styles.left}`}
+          
+          onMouseEnter={()=>setHoverLeft(true)} onMouseLeave={()=>setHoverLeft(false)}
+        >
+          <Image src={!hoverLeft ? arrow:arrowWhite}  className={`${styles.innerCircle} ${styles.leftIcon}`}  />
+          {/* <AiOutlineRightCircle size={40}  /> */}
+          <div className={`${styles.subDiv} ${styles.bottom_left}`}>
+     
+            <p>FOR THE FULL WEBSITE VISIT OUR</p>
+            <h4>BETA VERSION</h4>
+          </div>
+        </div>
+        <center>
+          <Image className={`${styles.overlay_image}`} src={palm} alt="" />
+        </center>
+        <div
+          className={`${styles.box} ${styles.right}`}
+          
+          onMouseEnter={()=>setHoverRight(true)} onMouseLeave={()=>setHoverRight(false)}
+        >
+          <Image src={!hoverRight ? arrow:arrowWhite} className={`${styles.innerCircle} ${styles.rightIcon}`} />
+           {/* <AiOutlineRightCircle className="innerCircle rightIcon" size={40}  /> */}
+          <div className={`${styles.subDiv} ${styles.bottom_right}`}>
+            <p>MINT NFTS WITH NO CODE ON OUR</p>
+            <h4>CREATOR STUDIO</h4>
+          </div>
         </div>
       </div>
+    </center>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    <footer>
+      <center>
+        <div className={`${styles.footer}`}>
+          JOIN THE OASIS
+        </div>
+      </center>
+      <div
+              className={`${styles.socials}`}
+              style={{ marginBottom: '20px' }}
+            >
+              <div
+                className={`${styles.socials_circle}`}
+                onClick={() =>
+                  window.open('https://twitter.com/oasisx_world', '_blank')
+                }
+              >
+                <FaTwitter size={20}  />
+              </div>
+              <div
+                className={`${styles.socials_circle}`}
+                onClick={() =>
+                  window.open('https://instagram.com/oasisx.world', '_blank')
+                }
+                style={{ marginLeft: '20px' }}
+              >
+                <FaInstagram size={24}  />
+              </div>
+              <div
+                className={`${styles.socials_circle}`}
+                onClick={() =>
+                  window.open(
+                    'https://www.linkedin.com/company/oasisx',
+                    '_blank',
+                  )
+                }
+                style={{ marginLeft: '20px' }}
+              >
+                < FaLinkedin size={24}  />
+              </div>
+            </div>
+    </footer>
+  </div>
+);
 }
